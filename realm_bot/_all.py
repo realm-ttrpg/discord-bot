b8ae09c466362c7f331702ca8a84138e3b570aa8
haliphax <haliphax@gmail.com>
2024-03-14 10:30:22 -0500

    remove setup/teardown of meta extension

--- realm_bot/_all.py
+++ realm_bot/_all.py
@@ -3,14 +3,3 @@
 META_EXTENSION = True
 
 _mods = ("roll",)
-_package = __name__.replace("._all", "")
-
-
-async def setup(bot):
-    for m in _mods:
-        await bot.load_extension(f"{_package}.{m}")
-
-
-async def teardown(bot):
-    for m in _mods:
-        await bot.unload_extension(f"{_package}.{m}")
